1dc1756b0d12348d51cafec51f707a6dca4549b9
mipunaruwan <nipunakalpa1234@gmail.com>
2022-06-22 02:54:35 +0530

    Init My my mvc sample project

--- test/src/model/OrderDetails.java
+++ test/src/model/OrderDetails.java
@@ -1,8 +1,40 @@
 package model;
 
 public class OrderDetails {
-   int orderid;
-   int item_code;
-   int orderqty;
-    double discount;
+   private int orderid;
+   private int item_code;
+   private int orderqty;
+    private double discount;
+
+    public int getOrderid() {
+        return orderid;
+    }
+
+    public void setOrderid(int orderid) {
+        this.orderid = orderid;
+    }
+
+    public int getItem_code() {
+        return item_code;
+    }
+
+    public void setItem_code(int item_code) {
+        this.item_code = item_code;
+    }
+
+    public int getOrderqty() {
+        return orderqty;
+    }
+
+    public void setOrderqty(int orderqty) {
+        this.orderqty = orderqty;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
 }
