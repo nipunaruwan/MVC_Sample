c0c6ededf8f778c29506a9599f31f3ca8f207f95
mipunaruwan <nipunakalpa1234@gmail.com>
2022-06-22 02:49:12 +0530

    Init My my mvc sample project

--- test/src/model/customer.java
+++ test/src/model/customer.java
@@ -2,13 +2,12 @@ package model;
 
 public class customer {
     int cusID;
+   String cust_title;
     String name;
     String addresss;
     String city;
     String provide;
     int postalcode;
-
-
 
 
 
--- test/src/model/item.java
+++ test/src/model/item.java
@@ -1,11 +1,59 @@
 package model;
 
 public class item {
-   int item_code;
-    String description;
-    int packSize ;
-    int qtyOnHand;
-    int unitPrice;
-    int item_type;
+   private int item_code;
+    private String description;
+    private int packSize ;
+    private int qtyOnHand;
+    private int unitPrice;
+    private int item_type;
 
+ public int getItem_code() {
+  return item_code;
+ }
+
+ public void setItem_code(int item_code) {
+  this.item_code = item_code;
+ }
+
+ public String getDescription() {
+  return description;
+ }
+
+ public void setDescription(String description) {
+  this.description = description;
+ }
+
+ public int getPackSize() {
+  return packSize;
+ }
+
+ public void setPackSize(int packSize) {
+  this.packSize = packSize;
+ }
+
+ public int getQtyOnHand() {
+  return qtyOnHand;
+ }
+
+ public void setQtyOnHand(int qtyOnHand) {
+  this.qtyOnHand = qtyOnHand;
+ }
+
+ public int getUnitPrice() {
+  return unitPrice;
+ }
+
+ public void setUnitPrice(int unitPrice) {
+  this.unitPrice = unitPrice;
+ }
+
+ public int getItem_type() {
+  return item_type;
+ }
+
+ public void setItem_type(int item_type) {
+  this.item_type = item_type;
+ }
+ 
 }
