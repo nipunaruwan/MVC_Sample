e7fa59a5d20117d81fef55d0c06768daa99aad07
mipunaruwan <nipunakalpa1234@gmail.com>
2022-06-22 01:05:51 +0530

    Init My my mvc sample project

--- test/src/model/customer.java
+++ test/src/model/customer.java
@@ -1,12 +1,12 @@
 package model;
 
 public class customer {
-    int cusID;
-    String name;
-    String addresss;
-    String city;
-    String provide;
-    int postcode;
+  //  int cusID;
+   // String name;
+  //  String addresss;
+   // String city;
+   // String provide;
+   // int postcode;
 
 
 
