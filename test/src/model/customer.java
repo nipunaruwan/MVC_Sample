f007999b55b54e4017eda675dddefff63d28b3ff
mipunaruwan <nipunakalpa1234@gmail.com>
2022-06-21 20:51:04 +0530

    Init My my mvc sample project

---
+++ test/src/model/customer.java
@@ -0,0 +1,17 @@
+package model;
+
+public class customer {
+    String cusID;
+    String name;
+
+    public customer(String cusID, String name) {
+        this.cusID = cusID;
+        this.name = name;
+    }
+
+
+    public customer() {
+    }
+
+
+}
