3300e09ffa493c82ecb5a08286797609cee645b0
mipunaruwan <nipunakalpa1234@gmail.com>
2022-06-21 20:59:37 +0530

    Init My my mvc sample project

--- test/src/model/customer.java
+++ test/src/model/customer.java
@@ -3,15 +3,10 @@ package model;
 public class customer {
     String cusID;
     String name;
-
-    public customer(String cusID, String name) {
-        this.cusID = cusID;
-        this.name = name;
-    }
+    String addresss;
+    
 
 
-    public customer() {
-    }
 
 
 }
