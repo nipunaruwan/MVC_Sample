dba38b067040d4e8a9b824d0dfe77e1f6010f50f
mipunaruwan <nipunakalpa1234@gmail.com>
2022-06-26 00:21:24 +0530

    Init My my mvc sample project

--- samepleMVC/src/controller/DashboardController.java
+++ samepleMVC/src/controller/DashboardController.java
@@ -1,4 +1,17 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class DashboardController {
+    public void btnorderdetails(ActionEvent actionEvent) {
+    }
+
+    public void btnorder(ActionEvent actionEvent) {
+    }
+
+    public void btnitem(ActionEvent actionEvent) {
+    }
+
+    public void btncustomer(ActionEvent actionEvent) {
+    }
 }
